5e272482f2015745911a5551ccfa8156edd39e1f
mihaicc30 <77832895+mihaicc30@users.noreply.github.com>
2022-02-21 21:59:03 +0000

    upd3

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mongoDBmihai";
+var http = require('http');
 
 
 
@@ -9,4 +10,13 @@ MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mongoDBmihai");
     console.log("DB conn successful!");
+    db.close();
   });
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  console.log("Website live");
+  res.end('Hello World! i am node js :3');
+}).listen(8080);
+
--- demo_create_collection.js
+++ demo_create_collection.js
@@ -1,9 +1,9 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mongoDBmihai";
+var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  var dbo = db.db("mongoDBmihai");
+  var dbo = db.db("mydb");
   dbo.createCollection("customers", function(err, res) {
     if (err) throw err;
     console.log("Collection created!");
